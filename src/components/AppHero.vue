a1f9887194b6d91dd2e825187a10107d8e3930d0
AndreaGarofalo <andreasjobs94@outlook.it>
2023-01-23 00:32:26 +0100

    added image component

---
+++ src/components/AppHero.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "The boys meme",
+    };
+  },
+};
+</script>
+
+<template>
+  <img src="../assets/Senza titolo.png" :alt="title" />
+</template>
+
+<style scoped>
+img {
+  width: 100%;
+  height: 80vh;
+}
+</style>
